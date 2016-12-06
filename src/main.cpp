f3742771237a529c70cd1b903d212eb1a325b7a7
jaro <jarohabiger@googleamil.com>
2016-12-06 21:56:14 +0100

    candles red

--- src/main.cpp
+++ src/main.cpp
@@ -12,10 +12,21 @@ void setup() {
 }
 
 void loop() {
-    uint32_t c;
-    c = strip.Color(5, 0, 0);
+    uint32_t red = strip.Color(10, 0, 0);
+    uint32_t off = strip.Color(0, 0, 0);
+    uint32_t yellow = strip.Color(20, 20, 0);
+
+
     for(uint16_t i=0; i < strip.numPixels(); i++) {
-            strip.setPixelColor(i, c);
+        if(i < 240-6 && i > 240 - 62) {
+          // first candle
+          strip.setPixelColor(i, red);
+        } else if(i < 240-70 && i > 240 - 126) {
+          // second candle
+          strip.setPixelColor(i, red);
+        } else {
+          strip.setPixelColor(i, off);
+        }
     }
     strip.show();
 }
