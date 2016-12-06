4413e41f717e738cef78de149fdd380301d39009
jaro <jarohabiger@googleamil.com>
2016-12-06 22:41:45 +0100

    add basic flickering

--- src/main.cpp
+++ src/main.cpp
@@ -14,19 +14,24 @@ void setup() {
 void loop() {
     uint32_t red = strip.Color(10, 0, 0);
     uint32_t off = strip.Color(0, 0, 0);
-    uint32_t yellow = strip.Color(25, 10, 0);
+
+    uint32_t flame[2];
+    for (size_t i = 0; i < 2; i++) {
+      int ran = random(10, 30);
+      flame[i] = strip.Color(ran, ran/2, 0);
+    }
 
 
     for(uint16_t i=0; i < strip.numPixels(); i++) {
         if(i < 240-53 && i > 240 - 62) {
           // first candle flame
-          strip.setPixelColor(i, yellow);
+          strip.setPixelColor(i, flame[0]);
         } else if(i < 240-2 && i > 240 - 52) {
           // first candle wax
           strip.setPixelColor(i, red);
         } else if(i < 240-68 && i > 240 - 77) {
           // second candle flame
-          strip.setPixelColor(i, yellow);
+          strip.setPixelColor(i, flame[1]);
         } else if(i < 240-78 && i > 240 - 126) {
           // second candle wax
           strip.setPixelColor(i, red);
@@ -36,4 +41,5 @@ void loop() {
         }
     }
     strip.show();
+    delay(random(75, 200));
 }
