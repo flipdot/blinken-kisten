f510f51bb194eb183a617c0ae0f763aae9f98998
jaro <jarohabiger@googleamil.com>
2016-12-06 21:36:41 +0100

    ficx

--- makefile
+++ makefile
@@ -1,3 +1,7 @@
 all:
 	platformio run
 
+upload-small:
+	platformio run -e nanoatmega168 -t upload --upload-port=/dev/ttyUSB0
+upload-big:
+	platformio run -e nanoatmega328 -t upload --upload-port=/dev/ttyUSB0
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
 
-#define PIN 0
+#define PIN 3
 
 // LEDS is the led count and defined in platformio.ini
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(LEDS, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(240, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
     strip.begin();
@@ -13,7 +13,7 @@ void setup() {
 
 void loop() {
     uint32_t c;
-    c = strip.Color(255, 0, 0);
+    c = strip.Color(5, 0, 0);
     for(uint16_t i=0; i < strip.numPixels(); i++) {
             strip.setPixelColor(i, c);
     }
