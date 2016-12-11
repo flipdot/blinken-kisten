bd8c1f380df52da565f51f97b7577435bf1a45af
malled2002 <malte@drabesch.de>
2016-12-12 00:32:08 +0100

    light up third candle

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #define PIN 3
 
 // LEDS is the led count and defined in platformio.ini
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(240, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(280, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
     strip.begin();
@@ -14,11 +14,18 @@ void setup() {
 void loop() {
     uint32_t red = strip.Color(10, 0, 0);
     uint32_t off = strip.Color(0, 0, 0);
+    uint32_t flash = strip.Color(255, 0, 142);
+    uint32_t green = strip.Color(0, 4, 0);
+    uint32_t green2 = strip.Color(0, 0, 6);
 
-    uint32_t flame[2];
+    uint32_t flame[4];
     for (size_t i = 0; i < 2; i++) {
       int ran = random(10, 30);
       flame[i] = strip.Color(ran, ran/2, 0);
+    }
+    for (size_t i = 2; i < 4; i++) {
+      int ran = random(10, 30);
+      flame[i] = strip.Color(ran, 0, ran/2);
     }
 
 
@@ -35,6 +42,16 @@ void loop() {
         } else if(i <= 240-78 && i > 240 - 126) {
           // second candle wax
           strip.setPixelColor(i, red);
+        } else if (i >= 241 && i <= 250){
+          // third candle wax
+          strip.setPixelColor(i, red);
+        } else if (i >= 251 && i <= 252){
+          strip.setPixelColor(i, flame[2]);
+        } else if (i >= 254 && i <= 266){
+          // fourth candle wax
+          strip.setPixelColor(i, red);
+        } else if (i > 266) {
+          strip.setPixelColor(i, green2);
         } else {
           // the rest
           strip.setPixelColor(i, off);
