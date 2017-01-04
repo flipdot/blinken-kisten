b6df364d73c5d31d9fc5c7d8fe50eceb616c70d9
Soeren Wegener <wegener92@gmail.com>
2017-01-04 22:57:39 +0100

    Programmed stuff that needs way too much space.

--- src/main.cpp
+++ src/main.cpp
@@ -11,57 +11,78 @@
 CHSV hsvs[NUM_LEDS];
 CRGB leds[NUM_LEDS];
 
+int kasten_position[] = {32, 61, 106, 149, 179, 280};
+uint8_t kasten_hue[] = {0, 0, 0, 0, 0, 0};
+
+void animation_rainbow() {
+    static uint8_t k = 0;
+    for (int i = 0; i < NUM_LEDS; i++) {
+        leds[i] = CHSV((i + k) % 256, 255, 255);
+    }
+    k++;
+}
+
+void animation_kasten_hue() {
+    uint8_t hue = 0;
+    uint8_t k = 0;
+    for (int i = 0; i < NUM_LEDS; i++) {
+        if (i == 0 || (k >= 0 && k < (int) sizeof(kasten_position) && i == kasten_position[k])) {
+            hue = kasten_hue[k];
+            hue += random(0, 4);
+            kasten_hue[k] = hue;
+            k++;
+            // this might be faster compared to modulo
+            if (k > sizeof(kasten_hue)) {
+                k = 0;
+            }
+        }
+        leds[i] = CHSV(hue, 255, 255);
+    }
+    delay(50);
+}
+
+void animation_kasten_blink() {
+    uint8_t hue = 0;
+    uint8_t k = 0;
+    for (int i = 0; i < NUM_LEDS; i++) {
+        if (i == 0 || (k >= 0 && k < (int) sizeof(kasten_position) && i == kasten_position[k])) {
+            hue = random(0, 256);
+            kasten_hue[k] = hue;
+            k++;
+            // this might be faster compared to modulo
+            if (k > sizeof(kasten_hue)) {
+                k = 0;
+            }
+        }
+        leds[i] = CHSV(hue, 255, 255);
+    }
+    delay(1000);
+}
+
 void setup() {
     FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
 }
 
 void loop() {
-    int kasten_position[] = {32, 61, 106, 149, 179, 280};
-    uint16_t kasten_hue[] = {0, 0, 0, 0, 0, 0};
-    int k = -1;
-    int active_kasten = -1;
-    uint16_t hue;
-    FastLED.show();
-    //hsvs[0].val = 255;
-    for (int i = 0; i < NUM_LEDS; i++) {
-        if (i == 0 || i == kasten_position[0] || i == kasten_position[1] || i == kasten_position[2] || i == kasten_position[3] || i == kasten_position[4]) {
-            hue = random(0, 256);
-            k++;
-        }
-        kasten_hue[k] = hue;
-        leds[i] = CHSV(hue, 255, 255);
-        //hsvs[i].setHSV(hue, 255, 255);
-    }
-    FastLED.show();
-    delay(1000);
+    // uses way too much space...
+    //uint8_t next_animation_counter = 100;
+    //void (*animations[3]) (void) = {
+    //    animation_rainbow,
+    //    animation_kasten_hue,
+    //    animation_kasten_blink,
+    //};
+    //uint8_t current_animation = random(0, sizeof(animations));
+    //current_animation = 0;
     while (true) {
-        k = -1;
-        for (int i = 0; i < NUM_LEDS; i++) {
-            if (i == 0 || i == kasten_position[0] || i == kasten_position[1] || i == kasten_position[2] || i == kasten_position[3] || i == kasten_position[4]) {
-                k++;
-                hue = kasten_hue[k];
-                hue += random(0, 5);
-                hue %= 256;
-                kasten_hue[k] = hue;
-            }
-            if (active_kasten == -1 || active_kasten == k) {
-                leds[i] = CHSV(hue, 255, 255);
-            } else {
-                leds[i] = CRGB(0, 0, 0);
-            }
-        }
-        if (active_kasten > -1) {
-            active_kasten = random(0, 7);
-            if (active_kasten >= 6) {
-                active_kasten = -1;
-            }
-            delay(200);
-        } else {
-            if (random(0, 1000) > 995) {
-                active_kasten = random(0, 6);
-            }
-        }
+        //next_animation_counter--;
+        //if (next_animation_counter == 0) {
+        //    current_animation = random(0, sizeof(animations));
+        //    next_animation_counter = random(100, 500);
+        //}
+        //current_animation = 0;
+        //animations[current_animation]();
+        animation_kasten_blink();
         FastLED.show();
-        delay(100);
+        delay(10);
     }
 }
