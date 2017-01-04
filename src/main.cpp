91fcdd339e72765435e46d547c944f2971eaf498
Soeren Wegener <wegener92@gmail.com>
2017-01-04 08:25:53 +0100

    Migrate adventskranz to blinken-kisten.

--- src/main.cpp
+++ src/main.cpp
@@ -1,78 +1,67 @@
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
+#include <FastLED.h>
+#define NUM_LEDS 280
+#define DATA_PIN 3
 
 #define PIN 3
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(280, PIN, NEO_GRB + NEO_KHZ800);
+// Adafruit_NeoPixel strip = Adafruit_NeoPixel(280, PIN, NEO_GRB + NEO_KHZ800);
+//
+CHSV hsvs[NUM_LEDS];
+CRGB leds[NUM_LEDS];
 
 void setup() {
-    strip.begin();
-    strip.show();
+    FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
 }
 
 void loop() {
-    uint32_t off = strip.Color(0, 0, 0);
-    uint32_t red = strip.Color(10, 0, 0);
-    uint32_t greenRgb = strip.Color(0, 5, 0);
-    uint32_t greenGrb = strip.Color(0, 0, 5);
-    uint32_t flash = strip.Color(255, 0, 142);
-    uint32_t flame[4];
-
-    for (size_t i = 0; i < 2; i++) {
-        int ran = random(10, 30);
-        flame[i] = strip.Color(ran, ran/2, 0);
-    }
-
-    for (size_t i = 2; i < 4; i++) {
-        int ran = random(10, 30);
-        flame[i] = strip.Color(ran, 0, ran/2);
-    }
-
-    for (uint16_t i = 0; i < strip.numPixels(); i++) {
-        if (i <= 240-125) {
-			// strip start
-            if (random(0, 200) == 42) {
-                strip.setPixelColor(i, flash);
-            } else {
-                strip.setPixelColor(i, greenRgb);
-            }
-        } else if (i >= 240-126 && i <= 240-78) {
-			// 2nd wax
-            strip.setPixelColor(i, red);
-        } else if (i >= 240-77 && i <= 240-68) {
-			// 2nd flame
-            strip.setPixelColor(i, flame[1]);
-        } else if (i >= 240-62 && i <= 240-53) {
-			// 1st flame
-            strip.setPixelColor(i, flame[0]);
-        } else if (i >= 240-52 && i <= 240-2) {
-			// 1st wax
-            strip.setPixelColor(i, red);
-        } else if (i >= 240+1 && i <= 240+10) {
-			// 3rd wax
-            strip.setPixelColor(i, red);
-        } else if (i >= 240+11 && i <= 240+12) {
-			// 3rd flame
-            strip.setPixelColor(i, flame[2]);
-        } else if (i >= 240+14 && i <= 240+15) {
-			// 4th flame
-            strip.setPixelColor(i, flame[3]);
-        } else if (i >= 240+16 && i <= 240+26) {
-			// 4th wax
-            strip.setPixelColor(i, red);
-        } else if (i >= 240+27) {
-			// strip end
-            if (random(0, 100) == 42) {
-                strip.setPixelColor(i, flash);
-            } else {
-                strip.setPixelColor(i, greenGrb);
-            }
-        } else {
-            // fallback
-            strip.setPixelColor(i, off);
+    int kasten_position[] = {32, 61, 106, 149, 179, 280};
+    uint16_t kasten_hue[] = {0, 0, 0, 0, 0, 0};
+    int k = -1;
+    int active_kasten = -1;
+    uint16_t hue;
+    FastLED.show();
+    //hsvs[0].val = 255;
+    for (int i = 0; i < NUM_LEDS; i++) {
+        if (i == 0 || i == kasten_position[0] || i == kasten_position[1] || i == kasten_position[2] || i == kasten_position[3] || i == kasten_position[4]) {
+            hue = random(0, 256);
+            k++;
         }
+        kasten_hue[k] = hue;
+        leds[i] = CHSV(hue, 255, 255);
+        //hsvs[i].setHSV(hue, 255, 255);
     }
-
-    strip.show();
-    delay(random(75, 200));
+    FastLED.show();
+    delay(1000);
+    while (true) {
+        k = -1;
+        for (int i = 0; i < NUM_LEDS; i++) {
+            if (i == 0 || i == kasten_position[0] || i == kasten_position[1] || i == kasten_position[2] || i == kasten_position[3] || i == kasten_position[4]) {
+                k++;
+                hue = kasten_hue[k];
+                hue += random(0, 5);
+                hue %= 256;
+                kasten_hue[k] = hue;
+            }
+            if (active_kasten == -1 || active_kasten == k) {
+                leds[i] = CHSV(hue, 255, 255);
+            } else {
+                leds[i] = CRGB(0, 0, 0);
+            }
+        }
+        if (active_kasten > -1) {
+            active_kasten = random(0, 7);
+            if (active_kasten >= 6) {
+                active_kasten = -1;
+            }
+            delay(200);
+        } else {
+            if (random(0, 1000) > 995) {
+                active_kasten = random(0, 6);
+            }
+        }
+        FastLED.show();
+        delay(100);
+    }
 }
